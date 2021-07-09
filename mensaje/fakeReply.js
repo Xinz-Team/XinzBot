de9d8396f133e32f8d614c0f41a7a941bc0a4bb7
Axel-bytu <86326762+Axel-bytu@users.noreply.github.com>
2021-07-09 15:45:44 -0500

    Update and rename message/fakeReply.js to mensaje/fakeReply.js

--- mensaje/fakeReply.js
+++ mensaje/fakeReply.js
@@ -34,7 +34,7 @@ exports.fakeToko = (fake, buffer = setting.pathImg) => {
 						"jpegThumbnail": buffer
 					},
 					"title": fake,
-					"description": "Self Aqulzz nih Boss",
+					"description": "Jefe de Self Aqulzz",
 					"currencyCode": "IDR",
 					"priceAmount1000": "50000000",
 					"retailerId": "Self Bot",
