ce2f52a84ed34cd9bba9bb310ad932171eb2428b
Axel-bytu <86326762+Axel-bytu@users.noreply.github.com>
2021-07-10 00:00:33 -0500

    Update and rename message/help.js to mensaje/help.js

--- mensaje/help.js
+++ mensaje/help.js
@@ -1,17 +1,24 @@
 exports.menu = (prefix, i) => {
+    
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
     return `*Xinz Bot*
-Created by Xinz Team
-
-*INFO*
-${i.free} cekprefix
-${i.free} ${prefix}stats
-${i.free} ${prefix}limit
-${i.free} ${prefix}balance
-${i.free} ${prefix}runtime
-${i.free} ${prefix}speed
-${i.free} ${prefix}owner
-${i.free} ${prefix}donasi
-${i.free} ${prefix}sourcecode
+    Created by Xinz Team
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
+         *Info*
+  ${i.free} cekprefix
+  ${i.free} ${prefix}stats
+  ${i.free} ${prefix}limit
+  ${i.free} ${prefix}balance
+  ${i.free} ${prefix}runtime
+  ${i.free} ${prefix}speed
+  ${i.free} ${prefix}owner
+  ${i.free} ${prefix}donasi
+  ${i.free} ${prefix}sourcecode
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 
 *Coverter / Tools*
 ${i.limit} ${prefix}sticker
@@ -26,7 +33,10 @@ ${i.limit} ${prefix}nuliskanan text
 ${i.limit} ${prefix}foliokiri text
 ${i.limit} ${prefix}foliokanan text
 ${i.limit} ${prefix}translate kodebahasa [reply pesan/text]
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Downloader*
 ${i.limit} ${prefix}ytmp4 url
 ${i.limit} ${prefix}ytmp3 url
@@ -36,11 +46,17 @@ ${i.limit} ${prefix}tiktok url
 ${i.limit} ${prefix}yts query
 ${i.limit} ${prefix}play query
 ${i.limit} ${prefix}playmp4 query
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Stalker*
 ${i.limit} ${prefix}igstalk username
 ${i.limit} ${prefix}ghstalk username
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Text Marker*
 ${i.limit} ${prefix}blackpink text
 ${i.limit} ${prefix}tahta text
@@ -48,7 +64,10 @@ ${i.limit} ${prefix}neon text
 ${i.limit} ${prefix}glitch text1|text2
 ${i.limit} ${prefix}thundername text
 ${i.limit} ${prefix}pornhub text1|text2
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Baileys*
 ${i.free} ${prefix}tagme
 ${i.limit} ${prefix}kontak nomor|nama
@@ -56,18 +75,27 @@ ${i.prem} ${prefix}hidetag
 ${i.free} ${prefix}jadian
 ${i.free} ${prefix}ganteng
 ${i.free} ${prefix}cantik
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Premium*
 ${i.owner} ${prefix}addprem @tag
 ${i.owner} ${prefix}delprem @tag
 ${i.owner} ${prefix}cekprem
 ${i.free} ${prefix}listprem
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Ban*
 ${i.owner} ${prefix}ban @tag
 ${i.owner} ${prefix}unban @tag
 ${i.free} ${prefix}listban
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Game*
 ${i.free} ${prefix}topbalance
 ${i.free} ${prefix}buylimit
@@ -75,7 +103,10 @@ ${i.free} ${prefix}buyglimit
 ${i.glimit} ${prefix}tictactoe @tag
 ${i.glimit} ${prefix}tebakgambar
 ${i.glimit} ${prefix}family100
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *VVIBU*
 ${i.limit} ${prefix}waifu
 ${i.limit} ${prefix}loli
@@ -83,7 +114,10 @@ ${i.limit} ${prefix}nekonime
 ${i.limit} ${prefix}megumin
 ${i.limit} ${prefix}sagiri
 ${i.limit} ${prefix}shinobu
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Random*
 ${i.free} ${prefix}apakah
 ${i.free} ${prefix}bisakah
@@ -94,7 +128,10 @@ ${i.free} ${prefix}cekbapak
 ${i.free} ${prefix}seberapagay
 ${i.limit} ${prefix}truth
 ${i.limit} ${prefix}dare
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Group*
 ${i.free} ${prefix}afk
 ${i.free} ${prefix}infogrup
@@ -114,7 +151,10 @@ ${i.owner} ${prefix}join
 ${i.owner} ${prefix}tagall
 ${i.owner} ${prefix}mute
 ${i.owner} ${prefix}unmute
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Enable / Disable*
 ${i.owner} ${prefix}antilink
 ${i.owner} ${prefix}welcome
@@ -123,7 +163,10 @@ ${i.owner} ${prefix}antibadword
 ${i.free} ${prefix}listbadword
 ${i.owner} ${prefix}addbadword
 ${i.owner} ${prefix}delbadword
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 *Owner*
 ${i.owner} >
 ${i.owner} $
@@ -136,11 +179,16 @@ ${i.owner} ${prefix}setprefix
 ${i.owner} ${prefix}bc
 ${i.owner} ${prefix}clearall
 ${i.owner} ${prefix}exif nama|author
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 
+┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
 Note :
 ${i.owner} : Fitur Admin Grup atau Owner
 ${i.free} : Fitur gratis yang tidak memakai limit
 ${i.limit} : Fitur yang memakai 1 limit harian
 ${i.glimit} : Fitur yang memakai 1 limit game
 ${i.prem} : Fitur khusus premium user`
+┆ 
+└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴✽̶⌣ ✽̶
 }
