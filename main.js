f6c4e94a3c21954191ba746002f255f9af379ca3
Axel-bytu <86326762+Axel-bytu@users.noreply.github.com>
2021-07-11 22:10:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,238 @@
-// Si desea editar en el mensaje/xinz.js
-'use strict';const _0x3f9f=['add','34721HqWBFc','jid','WAConnection','Maaf\x20bot\x20tidak\x20menerima\x20call','Connect,\x20Welcome\x20Owner','.json','\x27\x20Telah\x20berubah!','length','Terputus,\x20Alasan\x20:','key','11iIIVeN','CB:action,,call','existsSync','live','watchFile','./database/welcome.json','./message/group','cyan','stringify','value','default','merekam','Module\x20','Scan\x20QR\x20~~','getAfkPosition','./lib/afk','extendedText','./config.json','Xinz\x20Bot','replace','@adiwajshing/baileys','ws-close','507031OOIIMp','log','readFileSync','recording','from','cache','blockUser','connecting','./lib/myfunc','mengetik','./database/afk.json','91502cIOHHm','parse','split','Connecting...','blocklist','hasNewMessage','[\x20CREATED\x20BY\x20XINZTEAM\x20]','1SWlVpS','generate','writeFileSync','messages','1279764vbMdNq','baterai','push','open','base64EncodedAuthInfo','./message/help.js','splice','logger','\x20berhenti\x20afk,\x20dia\x20sedang\x20','./message/xinz.js','10izohYv','warn','Koneksi\x20terputus,\x20mencoba\x20menghubungkan\x20kembali..','resolve','connect','sendMessage','187301MVoIxT','85SgxCxK','checkAfkUser','3fkvIqN','135155zpilDI','CB:action,,battery','CB:Blocklist','1112780UYaQkK','\x20Dipantau\x20oleh\x20kang\x20Bakso','lastKnownPresence','qrcode-terminal','\x0aMencoba\x20mengkoneksi\x20ulang\x20:','presences'];const _0x5df51e=_0x49bf;(function(_0x16dec2,_0x284ba6){const _0x50d916=_0x49bf;while(!![]){try{const _0x334e3d=-parseInt(_0x50d916(0x1fc))*parseInt(_0x50d916(0x1e1))+parseInt(_0x50d916(0x1f5))*-parseInt(_0x50d916(0x1f8))+parseInt(_0x50d916(0x1e5))+parseInt(_0x50d916(0x1da))*-parseInt(_0x50d916(0x20d))+-parseInt(_0x50d916(0x1f9))*parseInt(_0x50d916(0x1ef))+parseInt(_0x50d916(0x1cf))+parseInt(_0x50d916(0x203))*parseInt(_0x50d916(0x1f6));if(_0x334e3d===_0x284ba6)break;else _0x16dec2['push'](_0x16dec2['shift']());}catch(_0x950f5b){_0x16dec2['push'](_0x16dec2['shift']());}}}(_0x3f9f,0xac32d));let {WAConnection:_WAConnection}=require('@adiwajshing/baileys'),{MessageType}=require(_0x5df51e(0x1cd));function _0x49bf(_0xd41642,_0x3debf3){_0xd41642=_0xd41642-0x1c8;let _0x3f9f06=_0x3f9f[_0xd41642];return _0x3f9f06;}const qrcode=require(_0x5df51e(0x1ff)),figlet=require('figlet'),fs=require('fs'),{color,XinzLog}=require('./lib/color'),{serialize}=require(_0x5df51e(0x1d7)),myfunc=require('./lib/myfunc'),afk=require(_0x5df51e(0x1c8));let WAConnection=myfunc[_0x5df51e(0x205)](_WAConnection),_afk=JSON[_0x5df51e(0x1db)](fs[_0x5df51e(0x1d1)]('./database/afk.json')),welcome=JSON[_0x5df51e(0x1db)](fs[_0x5df51e(0x1d1)](_0x5df51e(0x212))),left=JSON['parse'](fs['readFileSync']('./database/left.json')),setting=JSON['parse'](fs[_0x5df51e(0x1d1)](_0x5df51e(0x1ca))),blocked=[],baterai={'baterai':0x0,'cas':![]};require(_0x5df51e(0x1ee)),nocache(_0x5df51e(0x1ee),_0x306fe0=>console['log'](color('\x27'+_0x306fe0+_0x5df51e(0x209)))),require('./message/help.js'),nocache(_0x5df51e(0x1ea),_0x5e6ed1=>console[_0x5df51e(0x1d0)](color('\x27'+_0x5e6ed1+_0x5df51e(0x209))));const start=async(_0x22bc1c,_0x7c8e98=new WAConnection())=>{const _0x1656bd=_0x5df51e;_0x7c8e98[_0x1656bd(0x1ec)]['level']=_0x1656bd(0x1f0),console['log'](color(figlet['textSync'](_0x1656bd(0x1cb),{'font':'Standard','horizontalLayout':_0x1656bd(0x217),'vertivalLayout':_0x1656bd(0x217),'whitespaceBreak':![]}),_0x1656bd(0x214))),console['log'](color(_0x1656bd(0x1e0))),_0x7c8e98['on']('qr',_0x54f705=>{const _0x38b74a=_0x1656bd;qrcode[_0x38b74a(0x1e2)](_0x54f705,{'small':!![]}),console[_0x38b74a(0x1d0)](XinzLog(_0x38b74a(0x21a)));}),fs[_0x1656bd(0x20f)](_0x22bc1c)&&_0x7c8e98['loadAuthInfo'](_0x22bc1c),_0x7c8e98['on'](_0x1656bd(0x1d6),()=>{const _0x4ca530=_0x1656bd;console['log'](XinzLog(_0x4ca530(0x1dd)));}),_0x7c8e98['on'](_0x1656bd(0x1e8),_0x3ad07b=>{const _0x142816=_0x1656bd;console['log'](XinzLog(_0x142816(0x207)));}),await _0x7c8e98[_0x1656bd(0x1f3)]({'timeoutMs':0x1e*0x3e8}),fs['writeFileSync'](_0x22bc1c,JSON['stringify'](_0x7c8e98[_0x1656bd(0x1e9)](),null,'\x09')),_0x7c8e98['on'](_0x1656bd(0x1ce),()=>{const _0x5a7cc0=_0x1656bd;console['log'](XinzLog(_0x5a7cc0(0x1f1)));}),_0x7c8e98['on']('close',async({reason:_0x19eb11,isReconnecting:_0x2b7f87})=>{const _0x12125b=_0x1656bd;console['log'](XinzLog(_0x12125b(0x20b)+_0x19eb11+_0x12125b(0x200)+_0x2b7f87)),!_0x2b7f87&&console[_0x12125b(0x1d0)](XinzLog('Connect\x20To\x20Phone\x20Rejected\x20and\x20Shutting\x20Down.'));}),_0x7c8e98['on'](_0x1656bd(0x1fb),_0x5a836b=>{const _0x106dbb=_0x1656bd;if(blocked[_0x106dbb(0x20a)]>0x2)return;for(let _0x1babac of _0x5a836b[0x1][_0x106dbb(0x1de)]){blocked[_0x106dbb(0x1e7)](_0x1babac[_0x106dbb(0x1cc)]('c.us','s.whatsapp.net'));}}),_0x7c8e98['on'](_0x1656bd(0x20e),async _0x400f95=>{const _0xc91f84=_0x1656bd,_0x2a2837=_0x400f95[0x2][0x0][0x1][_0xc91f84(0x1d3)];_0x7c8e98[_0xc91f84(0x1f4)](_0x2a2837,_0xc91f84(0x206),MessageType['text']),await _0x7c8e98[_0xc91f84(0x1d5)](_0x2a2837,_0xc91f84(0x202));}),_0x7c8e98['on'](_0x1656bd(0x1fa),_0x44fc8a=>{const _0x3b70d9=_0x1656bd,_0x2366c9=_0x44fc8a[0x2][0x0][0x1][_0x3b70d9(0x216)],_0x40d237=_0x44fc8a[0x2][0x0][0x1][_0x3b70d9(0x210)];baterai[_0x3b70d9(0x1e6)]=_0x2366c9,baterai['cas']=_0x40d237;}),_0x7c8e98['on']('chat-update',async _0x35f508=>{const _0x1f7307=_0x1656bd;if(_0x35f508['presences'])for(let _0x251288 in _0x35f508[_0x1f7307(0x201)]){(_0x35f508[_0x1f7307(0x201)][_0x251288][_0x1f7307(0x1fe)]==='composing'||_0x35f508[_0x1f7307(0x201)][_0x251288][_0x1f7307(0x1fe)]===_0x1f7307(0x1d2))&&(afk[_0x1f7307(0x1f7)](_0x251288,_afk)&&(_afk[_0x1f7307(0x1eb)](afk[_0x1f7307(0x21b)](_0x251288,_afk),0x1),fs[_0x1f7307(0x1e3)](_0x1f7307(0x1d9),JSON[_0x1f7307(0x215)](_afk)),_0x7c8e98[_0x1f7307(0x1f4)](_0x35f508[_0x1f7307(0x204)],'@'+_0x251288[_0x1f7307(0x1dc)]('@')[0x0]+_0x1f7307(0x1ed)+(_0x35f508[_0x1f7307(0x201)][_0x251288][_0x1f7307(0x1fe)]==='composing'?_0x1f7307(0x1d8):_0x1f7307(0x218)),MessageType[_0x1f7307(0x1c9)],{'contextInfo':{'mentionedJid':[_0x251288]}})));}if(!_0x35f508[_0x1f7307(0x1df)])return;_0x35f508=_0x35f508[_0x1f7307(0x1e4)]['all']()[0x0];if(!_0x35f508['message'])return;if(_0x35f508[_0x1f7307(0x20c)]&&_0x35f508[_0x1f7307(0x20c)]['remoteJid']=='status@broadcast')return;let _0x2766da=serialize(_0x7c8e98,_0x35f508);require('./message/xinz')(_0x7c8e98,_0x2766da,blocked,baterai,_afk,welcome,left);}),_0x7c8e98['on']('group-participants-update',async _0x3d5243=>{const _0x378e2a=_0x1656bd;require(_0x378e2a(0x213))(_0x7c8e98,_0x3d5243,welcome,left);});};function nocache(_0x5bb51e,_0x249406=()=>{}){const _0x3bc396=_0x5df51e;console[_0x3bc396(0x1d0)](color(_0x3bc396(0x219)+_0x5bb51e+_0x3bc396(0x1fd))),fs[_0x3bc396(0x211)](require[_0x3bc396(0x1f2)](_0x5bb51e),async()=>{const _0x4d4ca3=_0x3bc396;await uncache(require[_0x4d4ca3(0x1f2)](_0x5bb51e)),_0x249406(_0x5bb51e);});}function uncache(_0x3288b7='.'){return new Promise((_0x3e11e5,_0x4a7b19)=>{const _0x1d5cb2=_0x49bf;try{delete require[_0x1d5cb2(0x1d4)][require[_0x1d5cb2(0x1f2)](_0x3288b7)],_0x3e11e5();}catch(_0x595784){_0x4a7b19(_0x595784);}});}start('./'+setting['sessionName']+_0x5df51e(0x208))['catch'](_0x3f24b3=>console[_0x5df51e(0x1d0)](_0x3f24b3));
+require('./config.js')
+const { WAConnection: _WAConnection } = require('@adiwajshing/baileys')
+const cloudDBAdapter = require('./lib/cloudDBAdapter')
+const { generate } = require('qrcode-terminal')
+const syntaxerror = require('syntax-error'
+const simple = require('./lib/simple'
+//  const logs = require('./lib/logs')
+const { promisify } = require('util')
+const yargs = require('yargs/yargs')
+const Readline = require('readline')
+const cp = require('child_process')
+const _ = require('lodash'
+const path = require('path'
+const fs = require('fs'
+var 
+try {
+  low = require('lowdb'
+} catch (e) {
+  low = require('./lib/lowdb')
+}
+const { Low, JSONFile } = low
+
+const rl = Readline.createInterface(process.stdin, process.stdout)
+const WAConnection = simple.WAConnection(_WAConnection)
+
+
+global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
+global.timestamp = {
+  start: new Date
+}
+// global.LOGGER = logs()
+const PORT = process.env.PORT || 3000
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+
+global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+
+global.db = new Low(
+  /https?:\/\//.test(opts['db'] || '') ?
+  new cloudDBAdapter(opts['db']) :
+  new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`)
+)
+global.DATABASE = global.db // Backwards Compatibility
+
+global.conn = new WAConnection()
+let authFile = `${opts._[0] || 'session'}.data.json`
+if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
+if (opts['trace']) conn.logger.level = 'trace'
+if (opts['debug']) conn.logger.level = 'debug'
+if (opts['big-qr'] || opts['server']) conn.on('qr', qr => generate(qr, { small: false }))
+if (!opts['test']) setInterval(async () => {
+  await global.db.write()
+}, 60 * 1000) // Save every minute
+if (opts['server']) require('./server')(global.conn, PORT)
+
+if (opts['test']) {
+  conn.user = {
+    jid: '2219191@s.whatsapp.net',
+    name: 'test',
+    phone: {}
+  }
+  conn.prepareMessageMedia = (buffer, mediaType, options = {}) => {
+    return {
+      [mediaType]: {
+        url: '',
+        mediaKey: '',
+        mimetype: options.mimetype || '',
+        fileEncSha256: '',
+        fileSha256: '',
+        fileLength: buffer.length,
+        seconds: options.duration,
+        fileName: options.filename || 'file',
+        gifPlayback: options.mimetype == 'image/gif' || undefined,
+        caption: options.caption,
+        ptt: options.ptt
+      }
+    }
+  }
+
+  conn.sendMessage = async (chatId, content, type, opts = {}) => {
+    let message = await conn.prepareMessageContent(content, type, opts)
+    let waMessage = await conn.prepareMessageFromContent(chatId, message, opts)
+    if (type == 'conversation') waMessage.key.id = require('crypto').randomBytes(16).toString('hex').toUpperCase()
+    conn.emit('chat-update', {
+      jid: conn.user.jid,
+      hasNewMessage: true,
+      count: 1,
+      messages: {
+        all() {
+          return [waMessage]
+        }
+      }
+    })
+  }
+  rl.on('line', line => conn.sendMessage('123@s.whatsapp.net', line.trim(), 'conversation'))
+} else {
+  rl.on('line', line => {
+    process.send(line.trim())
+  })
+  conn.connect().then(async () => {
+    await global.db.read()
+    global.db.data = {
+      users: {},
+      chats: {},
+      stats: {},
+      msgs: {},
+      sticker: {},
+      ...(global.db.data || {})
+    }
+    global.db.chain = _.chain(global.db.data)
+    fs.writeFileSync(authFile, JSON.stringify(conn.base64EncodedAuthInfo(), null, '\t'))
+    global.timestamp.connect = new Date
+  })
+}
+process.on('uncaughtException', console.error)
+// let strQuot = /(["'])(?:(?=(\\?))\2.)*?\1/
+
+let isInit = true
+global.reloadHandler = function () {
+  let handler = require('./xinz')
+  if (!isInit) {
+    conn.off('chat-update', conn.xinz)
+    conn.off('message-delete', conn.onDelete)
+    conn.off('group-participants-update', conn.onParticipantsUpdate)
+    conn.off('CB:action,,call', conn.onCall)
+  }
+  conn.welcome = 'Hola, @user!\nBienvenido al grupo @subject\n\n@desc'
+  conn.bye = 'Adiós @user!'
+  conn.spromote = '@user ahora es administrador!'
+  conn.sdemote = '@user ya no es administrador!'
+  conn.onParticipantsUpdate = xinz.participantsUpdate
+  conn.on('chat-update', conn.xinz)
+  conn.on('message-delete', conn.onDelete)
+  conn.on('group-participants-update', conn.onParticipantsUpdate)
+  conn.on('CB:action,,call', conn.onCall)
+  if (isInit) {
+    conn.on('error', conn.logger.error)
+    conn.on('close', () => {
+      setTimeout(async () => {
+        try {
+          if (conn.state === 'close') {
+            if (fs.existsSync(authFile)) await conn.loadAuthInfo(authFile)
+            await conn.connect()
+            fs.writeFileSync(authFile, JSON.stringify(conn.base64EncodedAuthInfo(), null, '\t'))
+            global.timestamp.connect = new Date
+          }
+        } catch (e) {
+          conn.logger.error(e)
+        }
+      }, 5000)
+    })
+  }
+  isInit = false
+  return true
+}
+
+// Plugin Loader
+let pluginFolder = path.join(__dirname, 'plugins')
+let pluginFilter = filename => /\.js$/.test(filename)
+global.plugins = {}
+for (let filename of fs.readdirSync(pluginFolder).filter(pluginFilter)) {
+  try {
+    global.plugins[filename] = require(path.join(pluginFolder, filename))
+  } catch (e) {
+    conn.logger.error(e)
+    delete global.plugins[filename]
+  }
+}
+console.log(Object.keys(global.plugins))
+global.reload = (_event, filename) => {
+  if (pluginFilter(filename)) {
+    let dir = path.join(pluginFolder, filename)
+    if (dir in require.cache) {
+      delete require.cache[dir]
+      if (fs.existsSync(dir)) conn.logger.info(`re - require plugin '${filename}'`)
+      else {
+        conn.logger.warn(`deleted plugin '${filename}'`)
+        return delete global.plugins[filename]
+      }
+    } else conn.logger.info(`requiring new plugin '${filename}'`)
+    let err = syntaxerror(fs.readFileSync(dir), filename)
+    if (err) conn.logger.error(`syntax error while loading '${filename}'\n${err}`)
+    else try {
+      global.plugins[filename] = require(dir)
+    } catch (e) {
+      conn.logger.error(e)
+    } finally {
+      global.plugins = Object.fromEntries(Object.entries(global.plugins).sort(([a], [b]) => a.localeCompare(b)))
+    }
+  }
+}
+Object.freeze(global.reload)
+fs.watch(path.join(__dirname, 'plugins'), global.reload)
+global.reloadHandler()
+
+
+
+// Quick Test
+async function _quickTest() {
+  let test = await Promise.all([
+    cp.spawn('ffmpeg'),
+    cp.spawn('ffprobe'),
+    cp.spawn('ffmpeg', ['-hide_banner', '-loglevel', 'error', '-filter_complex', 'color', '-frames:v', '1', '-f', 'webp', '-']),
+    cp.spawn('convert'),
+    cp.spawn('magick'),
+    cp.spawn('gm'),
+  ].map(p => {
+    return Promise.race([
+      new Promise(resolve => {
+        p.on('close', code => {
+          resolve(code !== 127)
+        })
+      }),
+      new Promise(resolve => {
+        p.on('error', _ => resolve(false))
+      })
+    ])
+  }))
+  let [ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm] = test
+  console.log(test)
+  let s = global.support = {
+    ffmpeg,
+    ffprobe,
+    ffmpegWebp,
+    convert,
+    magick,
+    gm
+  }
+  require('./lib/sticker').support = s
+  Object.freeze(global.support)
+
+  if (!s.ffmpeg) conn.logger.warn('Please install ffmpeg for sending videos (pkg install ffmpeg)')
+  if (s.ffmpeg && !s.ffmpegWebp) conn.logger.warn('Stickers may not animated without libwebp on ffmpeg (--enable-ibwebp while compiling ffmpeg)')
+  if (!s.convert && !s.magick && !s.gm) conn.logger.warn('Stickers may not work without imagemagick if libwebp on ffmpeg doesnt isntalled (pkg install imagemagick)')
+}
+
+_quickTest()
+  .then(() => conn.logger.info('Quick Test Done'))
+  .catch(console.error)
